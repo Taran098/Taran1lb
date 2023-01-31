f66936582daa982dafc56224ff17e0375887169d
Taran098 <122409989+Taran098@users.noreply.github.com>
2023-01-31 15:25:59 -0500

    Update StudentByAgeComparator.java

--- src/comparablestudent/StudentByAgeComparator.java
+++ src/comparablestudent/StudentByAgeComparator.java
@@ -13,11 +13,11 @@ public class StudentByAgeComparator implements Comparator<Student>{
     public int compare(Student student1, Student student2) {
         
         Integer age1 = student1.getAge();
-        Integer age2 = student2.getAge();
+        Integer age3 = student2.getAge();
         
         int returnValue = age1.compareTo(age3); // using compareTo from Integer
 
-//        int returnValue = age1 - age2; // in one step!
+//        int returnValue = age1 - age3; // in one step!
         
         return returnValue;
         
